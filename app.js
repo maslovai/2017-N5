0a9e74120e866e88606a1f42de92ff63d0c29d06
Pmookerjee <pmookerjee@gmail.com>
2017-02-04 12:28:45 -0800

    Added sumAndMultiply function

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ Write a function called sum() that takes in two numbers as arguments and then re
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
-// Return the sum of two numbers 
+// Return the sum of two numbers
 function sum(num1, num2){
   var sum = ' The sum of ' + num1 + ' and '+ num2 + ' is ' + (num1 + num2);
   return [(num1 + num2), sum];
 }
 
 // Here is the test for sum(); uncomment it to run it
-testSum(4, 7);
+//testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle, synchronize the code between both GitHubs and both laptops, and swap Driver/Navigator roles. Don't forget to create a new branch for your work on the next question!
 
@@ -41,7 +41,14 @@ Fourth element: "The product of 4 and 7 and 5 is 140."
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
-// Write your code here
+// Takes three numbers and returns the sum, product, and a string
+function sumAndMultiply(num1, num2, num3){
+  var sum = num1 + num2 + num3;
+  var product = num1 * num2 * num3;
+  //console.log([sum, product, num1 + ' and ' + num2 + ' and ' + num3 + ' sum to ' + sum + '.', 'The product of ' + num1 + ' and ' + num2 + ' and ' + num3 + ' is ' + product + '.']);
+  return [sum, product, num1 + ' and ' + num2 + ' and ' + num3 + ' sum to ' + sum + '.', 'The product of ' + num1 + ' and ' + num2 + ' and ' + num3 + ' is ' + product + '.'];
+}
+sumAndMultiply(4,7,5);
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
 //testSumAndMultiply(4,7,5);
